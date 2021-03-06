d0e52f50010002939bbff227e58fa8a778770333
arya2331 <rishabharya32@gmail.com>
2021-03-06 21:29:54 +0530

    Added functionality of uploading files

--- client/src/app/pages/pages-routing.module.ts
+++ client/src/app/pages/pages-routing.module.ts
@@ -5,6 +5,7 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ECommerceComponent } from './e-commerce/e-commerce.component';
 import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
+import { FileUploadComponent } from './files-upload/file-upload.component'; 
 
 const routes: Routes = [{
   path: '',
@@ -17,6 +18,10 @@ const routes: Routes = [{
     {
       path: 'iot-dashboard',
       component: DashboardComponent,
+    },
+    {
+      path: 'upload',
+      component: FileUploadComponent,
     },
     {
       path: 'layout',
--- client/src/app/pages/pages.module.ts
+++ client/src/app/pages/pages.module.ts
@@ -7,6 +7,7 @@ import { DashboardModule } from './dashboard/dashboard.module';
 import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+// import { FileUploadComponent } from './files-upload/file-upload.component'; 
 
 @NgModule({
   imports: [
