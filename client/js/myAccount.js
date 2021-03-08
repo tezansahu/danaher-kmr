d038fc8b06a0b1d7a397a0e3941625646082b870
tezansahu <tezansahu@gmail.com>
2021-03-08 23:10:30 +0530

    modified profile

--- client/js/myAccount.js
+++ client/js/myAccount.js
@@ -4,6 +4,9 @@ function doPatch(url, body, callback){
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
             callback(xmlHttp.responseText);
         }
+        if(xmlHttp.status == 400 || xmlHttp.status == 401){
+            callback(xmlHttp.status);
+        }
             
     }
     xmlHttp.open("PATCH", url, true); // true for asynchronous 
@@ -22,6 +25,7 @@ function populateProfile() {
 populateProfile()
 
 function updateProfile() {
+    document.getElementById("update_profile_spinner").style.display = "block";
     user = JSON.parse(window.localStorage.getItem("user"));
     user["op_co"] = document.getElementById("opco").value;
     user["contact_no"] = document.getElementById("contact").value;
@@ -35,6 +39,7 @@ function updateProfile() {
 
 
 function updatePassword() {
+    document.getElementById("update_passwd_spinner").style.display = "block";
     old_passwd = document.getElementById("passwd_old").value;
     new_passwd = document.getElementById("passwd_new").value;
 
@@ -61,9 +66,20 @@ function updatePassword() {
                 console.err(err);
             }
             else{
-                res = JSON.parse(res);
-                window.localStorage.setItem("user", JSON.stringify(res));
-                window.location.reload()
+                document.getElementById("update_passwd_spinner").style.display = "none";
+                if(res == 400){
+                    document.getElementById("passwordInvalid").innerHTML = "New password should not be same as old password";
+                    document.getElementById("passwordInvalid").style.display = 'block';
+                }
+                else if (res==401){
+                    document.getElementById("passwordInvalid").innerHTML = "Invalid Password. Please try again.";
+                    document.getElementById("passwordInvalid").style.display = 'block';
+                }
+                else{
+                    res = JSON.parse(res);
+                    window.localStorage.setItem("user", JSON.stringify(res));
+                    window.location.reload()
+                }
             }
         })
     }
